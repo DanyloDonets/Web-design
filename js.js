48d30fa2ba3abb95690837d7e8320c9a74d6743f
Danylo Donets <EndoBroYT@gmail.com>
2022-02-03 19:29:27 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,144 +1,4 @@
 /*
-let user ={
-	name: "John",
-	surname: "Smith"
-}
-user.name="Pete";
-delete user.name;
-console.log(user);
-
-isEmpty(user);
-
-function isEmpty(user){
-	let status="true"
-	for(let key in user){
-	if(typeof(user[key])!='undefined'){
-		status="false";
-	}
-}
-console.log(status);
-}
-
-let salaries={
-	John: 100,
-	Ann: 160,
-	Pete: 130
-}
-
-sum(salaries);
-
-function sum(obj){
-	let sum=0;
-	let text="Sum=";
-	for(let key in salaries){
-		if(typeof(salaries[key])=='number'){
-		sum= sum+salaries[key];
-	}
-}
-console.log(text+sum);
-}
-
-let menu={
-	width: 100,
-	height: 300,
-	title: "My menu"
-}
-
-multiplyNumeric(menu);
-function multiplyNumeric(obj){
-	for(let key in menu){
-		if(typeof(menu[key])=='number'){
-		menu[key]=menu[key]*2;
-	}
-}
-}
-console.log(menu);
-let obj={
-	name: "Ivanchenko",
-	m:function(){
-		console.log(this);
-	}
-}
-
-obj.m();
-
-function makeUser(){
-	return {
-		name: "asd",
-		surname: "dfs",
-		ref (){
-			return this;
-		}
-	};
-}
-let user1= makeUser();
-console.log(user1);
-
-// обьект калькуятор с 3 методами: read()– запрашивает 2 значения, sum()–сумировать, mul()-
-let calculator={
-	a: 0,
-	b: 0,
-	read:function(){
-		this.a=prompt("Vvedite znachenie",0);
-		this.b=prompt("Vvedite znachenie",0);
-	},
-	sum:function(){
-		let sum;
-		sum=this.a+this.b;
-		return sum;
-	},
-	mul:function(){
-		let mul;
-		mul=this.a*this.b;
-		return mul;
-	}
-}
-
-
-
-let ladder={
-	step: 0,
-	up:function(){
-		this.step=this.step+1;
-	},
-	down:function(){
-		this.step=this.step-1;
-	},
-	showStep:function(){
-		alert("Step="+this.step);
-		console.log("Step="+this.step);
-	}
-}
-function Calculator(a,b){
-	this.read=function(){
-		this.a=prompt("Vvedite znachenie",0);
-		this.b=prompt("Vvedite znachenie",0);
-	},
-	this.sum=function(){
-		let sum;
-		sum=this.a+this.b;
-		return sum;
-	},
-	this.mul=function(){
-		let mul;
-		mul=this.a*this.b;
-		return mul;
-	}
-}
-
-let constr = new Calculator(0,0);
-
-
-
-function Constructor(startingValue){
-	this.startingValue=startingValue,
-	this.startingValue += +prompt('Vvedste znachenie', 0);
-}
-
-let pr = new Constructor(15);
-
-console.log(pr.add());
-
 console.log(document.body.firstElementChild)
 console.log(document.body.lastElementChild.previousElementSibling)
 console.log(document.body.lastElementChild.previousElementSibling.lastElementChild)
@@ -148,12 +8,20 @@ console.log(document.body.children)
 console.log(document.body.firstElementChild)
 console.log(document.body.lastElementChild)
 */
-console.log(document.getElementById("age-table"))
-console.log(document.getElementById("age-table").querySelectorAll('label'))
-console.log(document.getElementById("age-table").querySelector('td'))
-console.log(document.querySelector('form[name=search]'))
-inputs = document.querySelector('form[name=search]').querySelectorAll('input')
-console.log(inputs[0])
-console.log(inputs[inputs.length-1])
+console.log("Task 1")
+	let lis = document.body.getElementsByTagName("li");
+
+
+	for (let i = 0; i < lis.length; i++) {
+
+	 	let num = lis[i].getElementsByTagName("li").length;
+	 	let name = lis[i].firstChild.nodeValue.trim();
+
+
+	 	console.log(`${name}: ${num}`);
+	}
+console.log("Task2")
+alert(document.body.lastChild.nodeType+", тип вузла елемента")
+
 
 
